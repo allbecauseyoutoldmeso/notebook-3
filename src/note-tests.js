8dc3d5732efc809dbc00f2371f54dbed0582b0b6
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-24 17:00:13 +0100

    create method to test including a variable in an array

--- src/note-tests.js
+++ src/note-tests.js
@@ -6,6 +6,19 @@ var assert = {
     } else
       console.log("It passed! You guys are awesome!");
   }
+};
+
+var check = {
+
+  toInclude: function(container, contained) {
+    if(!Array.isArray(container)) {
+      throw new Error(container + ' is not an array.  Oops.');
+    }
+    if(container.includes(contained)) {
+      console.log("It passed! You guys rock!");
+    } else
+      throw new Error("Unlucky! " + container + " did not contain " + contained);
+  }
 
 };
 
@@ -20,3 +33,16 @@ function testNoteHasText() {
 exports.testNoteHasText = testNoteHasText;
 
 })(this);
+
+(function(exports) {
+
+  function testCreateNote() {
+
+    var noteList = new NoteList();
+    notelist.createNote("Howdy!");
+
+
+  }
+
+
+});
